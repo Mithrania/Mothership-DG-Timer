ee5e5399448c97125e1d7b5588b81079d3e2b868
Mithrania <jack.currie@outlook.com>
2026-08-18 18:40:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crt-timer-v1';
+const CACHE_NAME = 'crt-timer-v2';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
